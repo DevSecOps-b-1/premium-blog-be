e6bfd40ddc04e72b5e0ec0689a3ac353e29e4cf9
RushingAlien <rushing27alien@gmail.com>
2024-11-11 19:38:52 +0700

    use file based secret

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,10 @@
 const Pool = require("pg").Pool;
+const  getSecret  = require("../utils/server/secret");
 require("dotenv").config();
 
 const pool = new Pool({
   user: process.env.PG_DB_USER,
-  password: process.env.PG_DB_PASSWORD,
+  password: getSecret("PG_DB_PASSWORD", "PG_DB_PASSWORD_FILE"),
   port: 5432,
   host: process.env.PG_DB_HOST,
   database: process.env.PG_DB_NAME,
