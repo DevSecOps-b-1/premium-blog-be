9a9252ab4250538f7e07ccf800c925413e9e876b
ariframaps <ariframa@student.ub.ac.id>
2024-11-18 15:30:35 +0700

    deleting unused controller and not limitting cors origin (for security misconfiguration)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 // Define CORS options
 const corsOptions = {
-  origin: "http://localhost:3000", // Allow only this origin
+  origin: "*", // Allow this origin. OWASP top ten
   methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
   credentials: true,
