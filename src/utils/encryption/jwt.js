e594b0a086966d7662118dc4bc3a12c73f8d9538
RushingAlien <rushing27alien@gmail.com>
2024-12-02 22:22:44 +0700

    Switch to file-based secret for jwt

--- src/utils/encryption/jwt.js
+++ src/utils/encryption/jwt.js
@@ -1,9 +1,12 @@
 const jwt = require("jsonwebtoken");
+const getSecret = "../server/secret"; 
+
+secretKey = getSecret("JWT_SECRET", "JWT_SECRET_FILE");
 
 module.exports.generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET);
+  return jwt.sign(payload, secretKey);
 };
 
 module.exports.verifyToken = (token) => {
-  return jwt.verify(token, process.env.JWT_SECRET);
+  return jwt.verify(token, secretKey);
 };
