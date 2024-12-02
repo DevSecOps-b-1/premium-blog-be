158b93ff0358e31d4acf96a4254a15c662fdd9f4
Ananda Fitra <anandadf@student.ub.ac.id>
2024-12-02 22:28:12 +0700

    fix(jwt): add require function

--- src/utils/encryption/jwt.js
+++ src/utils/encryption/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
-const getSecret = "../server/secret"; 
+const getSecret = require("../server/secret"); 
 
-secretKey = getSecret("JWT_SECRET", "JWT_SECRET_FILE");
+const secretKey = getSecret("JWT_SECRET", "JWT_SECRET_FILE");
 
 module.exports.generateToken = (payload) => {
   return jwt.sign(payload, secretKey);
